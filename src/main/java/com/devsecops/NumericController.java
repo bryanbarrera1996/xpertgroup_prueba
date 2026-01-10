bc561326b899a68fb4603c0ddd76bd50c7252824
Bryan Barrera Muñoz <bryanbarreramunoz@MacBook-Air-de-Bryan.local>
2026-01-10 00:03:55 -0500

    Fix sonar

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -3,47 +3,44 @@ package com.devsecops;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/")
 public class NumericController {
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private static final String baseURL = "http://node-service:5000/plusone";
-	//private static final String baseURL = "http://localhost:5000/plusone";
-	
-	RestTemplate restTemplate = new RestTemplate();
-	
-	@RestController
-	public class compare {
+    private static final Logger logger = LoggerFactory.getLogger(NumericController.class);
+    private static final String BASE_URL = "http://node-service:5000/plusone";
 
-		@GetMapping("/")
-		public String welcome() {
-			return "Kubernetes DevSecOps";
-		}
+    private final RestTemplate restTemplate;
 
-		@GetMapping("/compare/{value}")
-		public String compareToFifty(@PathVariable int value) {
-			String message = "Could not determine comparison";
-			if (value > 50) {
-				message = "Greater than 50";
-			} else {
-				message = "Smaller than or equal to 50";
-			}
-			return message;
-		}
+    public NumericController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
-		@GetMapping("/increment/{value}")
-		public int increment(@PathVariable int value) {
-			ResponseEntity<String> responseEntity = restTemplate.getForEntity(baseURL + '/' + value, String.class);
-			String response = responseEntity.getBody();
-			logger.info("Value Received in Request - " + value);
-			logger.info("Node Service Response - " + response);
-			return Integer.parseInt(response);
-		}
-	}
+    @GetMapping
+    public String welcome() {
+        return "Kubernetes DevSecOps";
+    }
 
+    @GetMapping("/compare/{value}")
+    public String compareToFifty(@PathVariable int value) {
+        if (value > 50) {
+            return "Greater than 50";
+        }
+        return "Smaller than or equal to 50";
+    }
+
+    @GetMapping("/increment/{value}")
+    public int increment(@PathVariable int value) {
+        logger.info("Value Received in Request - {}", value);
+
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                BASE_URL + "/" + value, String.class);
+
+        logger.info("Node Service Response - {}", response.getBody());
+
+        return Integer.parseInt(response.getBody());
+    }
 }
--- src/test/java/com/devsecops/NumericApplicationTest.java
+++ src/test/java/com/devsecops/NumericApplicationTest.java
@@ -1,11 +1,58 @@
 package com.devsecops;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
-class NumericApplicationTest {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(NumericController.class)
+class NumericControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RestTemplate restTemplate;
 
     @Test
-    void main_runs_without_exceptions() {
-        NumericApplication.main(new String[] {});
+    void welcome_shouldReturnMessage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Kubernetes DevSecOps"));
+    }
+
+    @Test
+    void compare_greaterThan50() throws Exception {
+        mockMvc.perform(get("/compare/60"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Greater than 50"));
+    }
+
+    @Test
+    void compare_smallerOrEqual50() throws Exception {
+        mockMvc.perform(get("/compare/10"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Smaller than or equal to 50"));
+    }
+
+    @Test
+    void increment_shouldReturnIncrementedValue() throws Exception {
+        Mockito.when(
+                restTemplate.getForEntity(
+                        "http://node-service:5000/plusone/5",
+                        String.class
+                )
+        ).thenReturn(ResponseEntity.ok("6"));
+
+        mockMvc.perform(get("/increment/5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("6"));
     }
 }
