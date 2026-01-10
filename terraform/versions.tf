c8ad9c5012c8c090dbcbf54b495890c619e0a445
Bryan Barrera Muñoz <bryanbarreramunoz@MacBook-Air-de-Bryan.local>
2026-01-09 22:55:24 -0500

    Upload terraform testing

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,16 +1,10 @@
-provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
+terraform {
+  required_version = ">= 1.5.0"
 
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    ec2 = "http://localhost:4566"
-    eks = "http://localhost:4566"
-    iam = "http://localhost:4566"
-    sts = "http://localhost:4566"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
