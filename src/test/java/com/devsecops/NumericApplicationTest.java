79af6645e2315bd8fc28b2885a5d7b7c110afbd7
Bryan Barrera Muñoz <bryanbarreramunoz@MacBook-Air-de-Bryan.local>
2026-01-09 23:40:20 -0500

    Fix coverage

---
+++ src/test/java/com/devsecops/NumericApplicationTest.java
@@ -0,0 +1,11 @@
+package com.devsecops;
+
+import org.junit.jupiter.api.Test;
+
+class NumericApplicationTest {
+
+    @Test
+    void main_runs_without_exceptions() {
+        NumericApplication.main(new String[] {});
+    }
+}
